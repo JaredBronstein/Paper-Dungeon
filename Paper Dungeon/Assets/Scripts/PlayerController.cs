8ba68c4403ad26e0d005f99851d02c16cfc8e683
Jonathan Way <jonathan.way@loop.colum.edu>
2019-09-19 09:40:53 -0500

    Game Template Part 1
    - Created 3 tilemaps for background, foreground, and borders
    
    - loaded in a temporary asset pack to use for the template (will be uninstalled when our assets are in)
    
    - created various folders for our assets
    
    - build an example level
    
    - started work on a player controller in command pattern style

---
+++ Paper Dungeon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    double playerSpeed;
+
+    Transform playerPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveUp()
+    {
+       // playerPosition.position.y += playerSpeed;
+    }
+
+    void MoveDown()
+    {
+
+    }
+
+    void MoveLeft()
+    {
+
+    }
+
+    void MoveRight()
+    {
+
+    }
+
+    void Interact()
+    {
+
+    }
+
+    void StartCombat(){ 
+    
+    }
+
+}
