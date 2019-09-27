5b748b4c5f2a020df08b33ac7b175e1b679917f0
Jonathan Way <jonathan.way@loop.colum.edu>
2019-09-27 14:54:37 -0500

    UI Work
    - Created My own scene to work in
    - Created a scen to work on the Combat UI
    - Created the start screen scene
    - created a simple UI for both the start screen and the Combat UI
    - made the combat UI a prefab to be used elsewhere

---
+++ Paper Dungeon/Assets/Scripts/LoadAScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAScene : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLoad;
+
+    void LoadTheScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
