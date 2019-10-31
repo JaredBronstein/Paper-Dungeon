0e4e00ea0b3a8f04a7bc7ca2aba13ff44af2d3c9
DutaiStudios <44248998+DutaiStudios@users.noreply.github.com>
2019-10-31 11:48:34 -0500

    InkPuzzle (Incomplete)
    Ink prefab
    Inkpuzzle 1 complete
    Inkpuzzle 2 layout w/ some working levers

---
+++ Paper Dungeon/Assets/Scripts/Puzzles/FinalPuzzle/Level1.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1 : MonoBehaviour
+{
+    public Sprite lever_0;
+    public Sprite lever_1;
+
+    public bool leverActivated;
+
+    public GameObject platform1;
+    public GameObject platform2;
+    public GameObject platform3;
+    public GameObject inkborders;
+    public SpriteRenderer leversprite;
+
+    private float moveleft = -.5f;
+    private float moveright = .5f;
+    private float borderreturn = -100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leversprite = GetComponent<SpriteRenderer>();
+        leversprite.sprite = lever_0;
+        leverActivated = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player in range");
+            if (Input.GetButtonDown("Activate"))
+            {
+                if (leverActivated == false)
+                {
+                    Debug.Log("Player activates lever");
+                    leverActivated = true;
+
+                }
+                else
+                {
+                    Debug.Log("Player turns off lever");
+                    leverActivated = false;
+
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (leverActivated == true)
+        {
+            leversprite.sprite = lever_1;
+
+
+        }
+        if (leverActivated == false)
+        {
+            leversprite.sprite = lever_0;
+        }
+
+
+    }
+}
