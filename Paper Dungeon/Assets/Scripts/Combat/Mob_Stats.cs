09f538cda129fbd90bdb497908d24867768d9d0b
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2019-10-31 21:24:51 -0500

    Updated Mob Stuff

--- Paper Dungeon/Assets/Scripts/Combat/Mob_Stats.cs
+++ Paper Dungeon/Assets/Scripts/Combat/Mob_Stats.cs
@@ -17,7 +17,14 @@ public class Mob_Stats : MonoBehaviour
 
     private void Awake()
     {
-        AttackList[0] = "Attack,true,2,0,1,true";
+        if(ATK > DEF)
+        {
+            AttackList[0] = "Attack,true,2,0,1,true";
+        }
+        else
+        {
+            AttackList[0] = "Magic Attack,true,4,0,1,false";
+        }
     }
     public string[] Attack(int AttackNumber)
     {
